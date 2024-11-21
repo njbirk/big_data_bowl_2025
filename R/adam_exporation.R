b1623af67880733112716c80bb8edeb549529ded
Adam Lynch <adamclynch03@gmail.com>
2024-11-21 16:12:28 -0500

    deleted

--- R/adam_exporation.R
+++
@@ -1,7 +0,0 @@
-# load the data
-plays <- read.csv("data/plays.csv")
-
-# def_cov <- subset(plays, select = c("pff_passCoverage", "gameId", "playId"))
-# player_play_cov <- merge(player_play, def_cov, by = c("gameId", "playId"))
-
-tapply(plays$time)
