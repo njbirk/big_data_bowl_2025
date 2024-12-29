37aff7182e5eba5b940b2edcad95a8285584f5f3
Adam Lynch <adamclynch03@gmail.com>
2024-12-29 10:25:08 -0500

    added setup function

--- misdirection_analysis/__init__.py
+++ misdirection_analysis/__init__.py
@@ -2,3 +2,4 @@ from .data import load_tracking_raw
 from .data import load_tracking_adjusted
 from .data import get_postion_groups
 from .motion_detection import append_motion_event
+from .data_setup import setup
