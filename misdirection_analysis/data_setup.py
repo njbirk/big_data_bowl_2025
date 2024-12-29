c6e92b4dedb8b8fcb20b817f2d47979fa35e1a92
Adam Lynch <adamclynch03@gmail.com>
2024-12-29 10:37:29 -0500

    setup function console print messages

--- misdirection_analysis/data_setup.py
+++ misdirection_analysis/data_setup.py
@@ -4,6 +4,13 @@ from . import motion_detection
 
 
 def setup():
+    print("This function will complete 3 progress bars.")
+    print("On Adam's MacBook Air M2:")
+    print("\tpbar1: 92 seconds")
+    print("\tpbar2: 91 seconds")
+    print("\tpbar3: 168 seconds")
+    print("\ttotal time: 5 minutes 51 seconds")
+
     # create the parqs folder if it does not exist
     if not os.path.exists(data._PARQ_DIR):
         os.mkdir(data._PARQ_DIR)
