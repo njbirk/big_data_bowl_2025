c347ab1742cdfa911921f205fa9f74d74803e406
Adam Lynch <adamclynch03@gmail.com>
2025-01-03 01:17:27 -0500

    updated init

--- misdirection_analysis/__init__.py
+++ misdirection_analysis/__init__.py
@@ -3,6 +3,5 @@ from .data import load_tracking_adjusted
 from .data import get_postion_groups
 from .motion_detection import append_motion_event
 from .data_setup import setup
-from .premotion_classify import create_motion_dataset
-from .premotion_classify import load_motion_dataset
 from .separation import append_separation_probability
+from .premotion_classify import append_premotion_classification
